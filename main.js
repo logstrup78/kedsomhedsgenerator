25eb3d1257876d9347177d83f559fada76fd1d6c
Karsten Løgstrup <karstenlogstrup@gmail.com>
2024-02-20 21:07:03 +0100

    Kedsomhedskappen 1.0

---
+++ main.js
@@ -0,0 +1,26 @@
+const kedsomhedsknap = document.querySelector('.trykbutton');
+const aktivitet = document.querySelector('.aktivitet');
+
+kedsomhedsknap.addEventListener('click', klik)
+
+function klik() {
+    fetch('aktiviteter.json')
+            .then(response => response.json())
+            .then(data => {
+                const aktiviteter = data.aktiviteter;
+                const randomIndex = Math.floor(Math.random() * aktiviteter.length);
+                const randomActivity = aktiviteter[randomIndex];
+                aktivitet.textContent = `${randomActivity}`;
+            })
+            .catch(error => console.error('Fejl ved indlæsning af aktiviteter:', error));
+            kedsomhedsknap.style.display = 'none';
+
+            // Nulstil resultatet efter 3 sekunder
+            setTimeout(() => {
+                aktivitet.textContent = '';
+                // Genopret knappens synlighed efter yderligere 1 sekund
+                setTimeout(() => {
+                    kedsomhedsknap.style.display = 'block';
+                }, 1000);
+            }, 3000);
+    };
